f4f12b87e4495cb3a46ef44ccbce77f6e524bbb6
ArgusCalonDev <putuardhya06@gmail.com>
2025-12-12 19:28:24 +0800

    tambahkan collisian

--- diedomer/player.gd
+++ diedomer/player.gd
@@ -8,7 +8,6 @@ func _physics_process(delta: float) -> void:
     #terapkan gravitasi sesuai arah
     velocity += gravity_dir * gravity * delta
     move_and_slide()
-    print($".".global_position)
     
 func _on_buttonrgt_button_down() -> void:
     velocity.x += 200
@@ -24,10 +23,5 @@ func _on_buttonjmp_pressed() -> void:
 
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
-    if area.name == "parkour":
-        gravity_dir = Vector2.ZERO
-
-
-func _on_area_2d_area_exited(area: Area2D) -> void:
-    if area.name == "parkour":
-        gravity_dir = Vector2.DOWN
+    if area.is_in_group("jebakan"):
+        queue_free()
