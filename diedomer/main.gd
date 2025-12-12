ce0b2e5eed66513e2d957f7d61de5ef61a98f723
ArgusCalonDev <putuardhya06@gmail.com>
2025-12-12 19:55:46 +0800

    tambahkan menu awal, dan optimalisasi

--- diedomer/main.gd
+++ diedomer/main.gd
@@ -1,1 +1,17 @@
 extends Node2D
+
+@onready var menu_awal = $MenuAwal
+
+func _ready() -> void:
+    get_tree().paused = true
+    menu_awal.PROCESS_MODE_ALWAYS
+    menu_awal.visible = true
+
+
+func _on_mulai_pressed() -> void:
+    get_tree().paused = false
+    menu_awal.visible = false
+
+func _on_balik_ke_mainmenu_pressed() -> void:
+    get_tree().paused = false
+    pass
--- diedomer/player.gd
+++ diedomer/player.gd
@@ -3,8 +3,8 @@ extends CharacterBody2D
 var gravity = 1200
 var jump_force = -450
 var gravity_dir = Vector2.DOWN # gravitasi kebawah
-var speed := 1.5
-var move_dir = 0.5
+var speed := 2.5
+var move_dir = 0.75
 
 func _physics_process(delta: float) -> void:
     #terapkan gravitasi sesuai arah
@@ -12,15 +12,7 @@ func _physics_process(delta: float) -> void:
     velocity.x += speed * move_dir
     move_and_slide()
     
-func _on_buttonrgt_button_down() -> void:
-    velocity.x += 200
 
-
-func _on_buttonrgt_button_up() -> void:
-    velocity.x += 0
-    
-
-    
 func _on_buttonjmp_pressed() -> void:
     velocity.y = jump_force
 
