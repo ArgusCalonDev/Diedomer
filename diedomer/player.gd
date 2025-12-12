7dca6430faa2aede485cf0e040d26d47d060fcdc
ArgusCalonDev <putuardhya06@gmail.com>
2025-12-12 18:42:50 +0800

    benerin logika pergerakan sumbu x

--- diedomer/player.gd
+++ diedomer/player.gd
@@ -8,21 +8,16 @@ func _physics_process(delta: float) -> void:
     #terapkan gravitasi sesuai arah
     velocity += gravity_dir * gravity * delta
     move_and_slide()
+    print($".".global_position)
+    
+func _on_buttonrgt_button_down() -> void:
+    velocity.x += 200
 
 
-func _on_buttonrgt_pressed() -> void:
-    velocity.x = 200
+func _on_buttonrgt_button_up() -> void:
+    velocity.x += 0
+    
 
-
-func _on_buttonrgt_released() -> void:
-    velocity.x = 0
-
-
-func _on_buttondlft_pressed() -> void:
-    velocity.x = -200
-
-func _on_buttondlft_released() -> void:
-    velocity.x = 0
     
 func _on_buttonjmp_pressed() -> void:
     velocity += -gravity_dir.normalized() * 450
@@ -38,3 +33,11 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 func _on_area_2d_area_exited(area: Area2D) -> void:
     if area.name == "parkour":
         gravity_dir = Vector2.DOWN
+
+
+
+func _on_buttondlft_button_down() -> void:
+    velocity.x += -200
+
+func _on_buttondlft_button_up() -> void:
+    velocity.x += 0
