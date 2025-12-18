e40d1fa45a043859e028a8fe946dfb72738424de
ArgusCalonDev <putuardhya06@gmail.com>
2025-12-18 19:21:03 +0800

    update lokal projek

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 **Diedomer** adalah game flappy-style yang memiliki rintangan rintangan seperti "lorong" di atas dan bawah, serta rintangan rintangan lainnya yang saya sebut sebagai 
 "rintangan jumscare".
 
-Repository ini untuk orang yang mau modifikasi game Diedomer buatan saya (open source!). Versi: **hfd-prototype-1.** 
+Repository ini untuk orang yang mau modifikasi game Diedomer buatan saya (open source!). Versi: **Alpha LE-1.** 
 
 **Cara modifikasi**: Klik tombol "fork" di bagian atas halaman repository. Lalu lanjutkan proses fork. Setelah berhasil, anda bisa **clone repo**, lalu **buka folder "diedomer" yang ada
 di folder repository ke godot engine**. **PENTING SEKALI: godot anda sebaiknya memiliki versi 4.4 atau 4.4.1. Jika tidak maka kemungkinan proses tidak berjalan lancar.**.
--- diedomer/player.gd
+++ diedomer/player.gd
@@ -3,24 +3,39 @@ extends CharacterBody2D
 var gravity = 1200
 var jump_force = -450
 var gravity_dir = Vector2.DOWN # gravitasi kebawah
-var speed := 2.5
-var move_dir = 0.5
 
+var speed := 300.0 # kecepatan awal
+var max_speed := 2000.0 #maksimum kecepatan
+var speed_step := 60.0 # speed bertambah tiap ...
+var speed_timer := 0.0 # speed timer
+var move_dir = 1.0
+# nodes
+@onready var speed_label = $"../CanvasLayer/speed"
 
-func _physics_process(delta: float) -> void:
-    #terapkan gravitasi sesuai arah
+func _physics_process(delta):
     velocity += gravity_dir * gravity * delta
-    velocity.x += speed * move_dir
+    # atur pertambahan speed
+    speed_timer += delta
+    if speed_timer >= 1.5:
+        speed += speed_step
+        speed = min(speed, max_speed)
+        speed_timer = 0.0
+        update_speed_label()
+
+    velocity.x = speed
     move_and_slide()
     
-
 func _on_buttonjmp_pressed() -> void:
     velocity.y = jump_force
 
-
 func _on_area_2d_area_entered(area: Area2D) -> void:
     if area.is_in_group("jebakan"):
-        queue_free()
+        Engine.time_scale = 0.2
+        await get_tree().create_timer(0.2).timeout
+        Engine.time_scale = 1
         get_tree().change_scene_to_file("res://you_lose.tscn")
     if area.is_in_group("win area"):
         get_tree().change_scene_to_file("res://you_win.tscn")
+
+func update_speed_label() -> void:
+    speed_label.text = "speed: " + str(speed)
